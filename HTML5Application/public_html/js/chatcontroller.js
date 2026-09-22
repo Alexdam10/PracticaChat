f3353dec41cac84614a36512f44366ef1e5fd30a
Jairubre <jairubre@gmail.com>
2026-09-22 12:39:55 +0200

    Trabajo de casa pagina web

---
+++ HTML5Application/public_html/js/chatcontroller.js
@@ -0,0 +1,15 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+class chatcontroller{
+    contructor(){
+        this.lista=[];
+    }
+    
+    recogertexto(){
+        mostrartext=document.getElementById("miCuadro");
+        cogertext=document.getElementById("cuadroEscritura");
+    }
+}
+
---
+++ HTML5Application/public_html/js/mensaje.js
@@ -0,0 +1,20 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+class mensaje{
+        
+        texto="";
+        fecha;
+        
+        constructor(texto){
+            this.texto=texto;
+            this.fecha=new Date();
+        }
+        
+        get texto(){
+            return this.texto;
+        }
+}
+
