c0bdab8fa62afaf7b8ba20aa989dcf8eb9d7776d
roswer <roswer.lopez@educa.madrid.org.com>
2026-09-22 10:45:04 +0200

    js y html

---
+++ HTML5Application/public_html/js/mensaje.js
@@ -0,0 +1,14 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+class mensaje{
+   /*
+    * atributo de texto
+    * @type String
+    */
+    texto="";
+   
+    
+}
+
