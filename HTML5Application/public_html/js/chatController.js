70da108e5b9fb9b8ac971add9cf526c9cd9e19c6
roswer <roswer.lopez@educa.madrid.org.com>
2026-09-23 11:08:24 +0200

    js y html

--- HTML5Application/public_html/js/chatController.js
+++ HTML5Application/public_html/js/chatController.js
@@ -2,8 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
-function actualizarMensajes(mensaje){
-    
-    
-}
 
+/*esta funcion muestra los mensajes del parametro
+ * en un pagina en forma de texto dentro 
+ * @param {type} mensaje
+ * @returns {undefined}
+ */
+function actualizarMensajes(mensaje){
+   //ordenar mensaje por fecha de mensaje de forma 
+   //que siempre sea de reciente a antiguo(en cada actualizacion)
+    
+   //recorrer la coleccion de mensajes(scroll)
+   //en cada iteraccion añadimos al elemento <div> contenido   
+   //consiste en el tecto del mensaje dentro de un elemeto(dinamico)
+}
+function enviarMensajes(){
+    //recibe el texto <textarea>
+    //enviar el mensaje con la hora que se envia
+    //añades a la coleccion
+}
--- HTML5Application/public_html/js/mensaje.js
+++ HTML5Application/public_html/js/mensaje.js
@@ -3,12 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
 class mensaje{
-   /*
-    * atributo de texto
-    * @type String
-    */
+   /*atributo de texto @type String*/
     texto="";
-   
-    
+    /*atributo tipo tiempo*/
+   fechaHora;
+  
+    constructor(texto = "") {
+    this.texto = texto;
+    this.fechaHora = new Date(); // Asigna la fecha y hora actual al crearse
+  }
+ obtenerHoraFormateada() {
+    return this.fechaHora.toLocaleTimeString(
+            [], {hour: "2-digit",minute: "2-digit"});
+  }
 }
 
