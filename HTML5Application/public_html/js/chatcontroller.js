30099c40ff742f5ffaf931dfe7980203529796ab
Alex <alexpaezminguez10@gmail.com>
2026-09-23 12:55:16 +0200

    funciones enviarmensaje y actualizar mensaje completas

--- HTML5Application/public_html/js/chatcontroller.js
+++ HTML5Application/public_html/js/chatcontroller.js
@@ -2,21 +2,26 @@
 Script chat controlle 
 
  */
+/**Array de mensaje*/ // variable global
+
+var mensajes=new Array();
 /**
  * muestra todos los mensajes del paramentro  
  * pagina de forma de texto dentro del div 
  * @param {type} mensajes de coleccion para mostrar los mensajes 
  * @returns {undefined}
  */
-function actualizarMEnsajes(mensajes){
+function actualizarMensajes(mensajes){
     // ordenar los mensajes por fecha de mensaje de forma 
     // mas reciente a mas antiguo. 
+    
     // despues recorremos la coleccion de mensajes
     
             // en cada iteracion añadimos al elemento <DIV> contenido 
             // consistente en el texto del mensaje, dentro de un elemento<li> 
             
-            
+            }    
+   
    
 /**
  * funcion que recoje los mensajes del uysuario y lo manda a la coleccion.
@@ -28,10 +33,22 @@ function actualizarMEnsajes(mensajes){
             
  function enviarMensaje(mensaje){
      // Recibe el texto del usuario 
+     let mensaje=document.getElementById("msgText").value;
+     
      // lo añades a la coleccion de mensaje con la hora 
+     mensajes.add(new mensaje (mensaje,Date.now()));
      
      
      
  }
     
-}
+
+
+//asocio la funcion actualizar mensaje como manejadora del eveento .
+//Dom de la pagina. 
+
+document.addEventListener("DOMContentloaded", actualizarMensajes());
+
+//asocio la funcion ennviar mensaje como manejadora del evento.
+//click del evento send buttonn. 
+document.getElementById("senddbutton").addEventListenner('click', enviarMensaje);
--- HTML5Application/public_html/js/mensaje.js
+++ HTML5Application/public_html/js/mensaje.js
@@ -3,4 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
 
-
+class Mensaje{
+    // añidimos un metodo para ordenar los mensajes y la hora 
+    
+    texto="";
+    fecha;
+    
+    
+}
