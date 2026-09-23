e5363806ce4c65fdc602b45feb12fe49c8f18695
Alex <alexpaezminguez10@gmail.com>
2026-09-23 11:08:01 +0200

    Funciones actualizarMEnsajes y enviarMensajes

--- HTML5Application/public_html/js/chatcontroller.js
+++ HTML5Application/public_html/js/chatcontroller.js
@@ -1,6 +1,37 @@
 /* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+Script chat controlle 
+
  */
-
-
+/**
+ * muestra todos los mensajes del paramentro  
+ * pagina de forma de texto dentro del div 
+ * @param {type} mensajes de coleccion para mostrar los mensajes 
+ * @returns {undefined}
+ */
+function actualizarMEnsajes(mensajes){
+    // ordenar los mensajes por fecha de mensaje de forma 
+    // mas reciente a mas antiguo. 
+    // despues recorremos la coleccion de mensajes
+    
+            // en cada iteracion añadimos al elemento <DIV> contenido 
+            // consistente en el texto del mensaje, dentro de un elemento<li> 
+            
+            
+   
+/**
+ * funcion que recoje los mensajes del uysuario y lo manda a la coleccion.
+ * pagina de forma de texto dentro del div 
+ * @param {type} mensaje de para añadir a la colecicon 
+ * @returns {undefined}
+ */         
+            
+            
+ function enviarMensaje(mensaje){
+     // Recibe el texto del usuario 
+     // lo añades a la coleccion de mensaje con la hora 
+     
+     
+     
+ }
+    
+}
