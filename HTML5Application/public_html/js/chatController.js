ca2295dbf0d3c2eef72d194c70b65bd721ec7962
roswer <roswer.lopez@educa.madrid.org.com>
2026-09-23 12:31:21 +0200

    js y html actualizacion 1

--- HTML5Application/public_html/js/chatController.js
+++ HTML5Application/public_html/js/chatController.js
@@ -3,21 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
 
+//ARRAY DE MENSAJES
+//declarar variable var,let,const(no se puede reafirmar)
+var mensaje = new Array();
 /*esta funcion muestra los mensajes del parametro
  * en un pagina en forma de texto dentro 
  * @param {type} mensaje
  * @returns {undefined}
  */
-function actualizarMensajes(mensaje){
-   //ordenar mensaje por fecha de mensaje de forma 
-   //que siempre sea de reciente a antiguo(en cada actualizacion)
-    
-   //recorrer la coleccion de mensajes(scroll)
-   //en cada iteraccion añadimos al elemento <div> contenido   
-   //consiste en el tecto del mensaje dentro de un elemeto(dinamico)
+function actualizarMensajes() {
+    //ordenar mensaje por fecha de mensaje de forma 
+    //que siempre sea de reciente a antiguo(en cada actualizacion)
+
+    //recorrer la coleccion de mensajes(scroll)
+    //en cada iteraccion añadimos al elemento <div> contenido   
+    //consiste en el tecto del mensaje dentro de un elemeto(dinamico)
 }
-function enviarMensajes(){
-    //recibe el texto <textarea>
-    //enviar el mensaje con la hora que se envia
-    //añades a la coleccion
+function enviarMensajes() {
+//recibe el texto <textarea>
+    let textoMensaje = document.getElementById("msgText").value;
+//enviar el mensaje con la hora que se envia
+//añades a la coleccion
+    mensaje.add(new Mensaje(mensaje,new Date));
 }
+//asociar un listener
+//cuando pulsas click en el boton se envia el mensaje
+document.getElementById("sendButton").addEventListener('click', enviarMensajes);
+//asocio el add event   
+//actualizar se asocia con el evento de carga del DOM de la pagina 
+document.addEventListener('DOMContentLoaded', actualizarMensajes(mensaje));
