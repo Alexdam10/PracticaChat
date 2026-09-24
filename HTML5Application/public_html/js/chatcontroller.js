b7d5b3e8cb571cfb34f2ad2aa5b505b5a2f21bc1
jairubre <jairubre@gmail.com>
2026-09-24 16:24:29 +0200

    Array dado la vuelta para los mensajes

--- HTML5Application/public_html/js/chatcontroller.js
+++ HTML5Application/public_html/js/chatcontroller.js
@@ -6,32 +6,41 @@
 import {mensaje} from './mensaje.js'
 
 //Array de javaScript 
-var mensajes = new Array();
+        var mensajes = new Array();
 //mensasjes es la colección de mensajes a mostrrar
 
 function actualizarMensajes() {
     //alert("Estas dentro de actualizar");
     //ordenar los mensajes de mas recciente a mas antiguo
 
-
-
-
     //Localizamos el elemento donde vamos a poner la ista de los mensajes
     let lstUl = document.getElementById("msglist");
+    //Sirve para limpiar el ul cada vez q   ue hacemos una interración con el bucle
     lstUl.innerHTML = "";
+    //Nos creamos un documento li para poder escribir lo q tenemos guardado en el array
 
 
 
     //Recorrer la colección de mensajes  
-    for (let msg of mensajes) {
-        //Nos creamos un documento li para poder escribir lo q tenemos guardado en el array
+    /*  for (let msg of mensajes) {
+     
+     //Llamamos al onbejto li para poder escribir el array en el cuadro 
+     li.textContent = msg.gettexto + "  " + msg.getfecha;
+     
+     lstUl.appendChild(li);
+     console.log(msg.texto + "  " + msg.fecha);
+     //Recorremos el array de manera inversa
+     //Primero tenemos q saber cuantas posiciones tiene
+     
+     }*/
+
+    let contArray = mensajes.length;
+    console.log("Este es el valor de array " + contArray);
+    for (var i = contArray - 1; i >= 0; i--) {
         let li = document.createElement("li");
-        li.textContent = msg.gettexto + "  " + msg.getfecha;
-        //
+        li.textContent = mensajes[i].gettexto + " " + mensajes[i].getfecha;
         lstUl.appendChild(li);
-        console.log(msg.texto + "  " + msg.fecha);
-
-
+        console.log("hola");
     }
 
     // En cada interracion añadimos al elemento <div> contenido
@@ -47,18 +56,19 @@ function enviarMensaje() {
     let texto = document.getElementById("msgText").value;
 
     // lo añadimos a la coleccion de mensajes
-   // mensajes.push(new mensaje(texto, Date.now()));
+    // mensajes.push(new mensaje(texto, Date.now()));
     //Array de javi de clase
     mensajes.push(new mensaje(texto, new Date()));
     //HAcemos un atributo con el text area y lo limpiamos con el value
-    let escritura=document.getElementById("msgText");
-    escritura.value="";
+    let escritura = document.getElementById("msgText");
+    //Lo usamos para limpiar la escritura cada vez q le damos al boton de enviar
+    escritura.value = "";
     escritura.focus();
     /*for (let msg of mensajes) {
-        //alert("Estas dentro del bucle");
-        //console.log(msg.texto + "  " + msg.fecha);
-
-    }*/
+     //alert("Estas dentro del bucle");
+     //console.log(msg.texto + "  " + msg.fecha);
+     
+     }*/
 
     actualizarMensajes();
 }
