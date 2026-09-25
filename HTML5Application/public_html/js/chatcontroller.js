4644f55b2013aea69f7a4312d123c97c8c5a93b4
Alex <alexpaezminguez10@gmail.com>
2026-09-25 09:38:09 +0200

    Ultima actualizacion chatcontroller y mensaje

--- HTML5Application/public_html/js/chatcontroller.js
+++ HTML5Application/public_html/js/chatcontroller.js
@@ -2,6 +2,8 @@
 Script chat controlle 
 
  */
+//sirve para importar la clase mensaje y que sepa que es lo que tiene que utilizar.
+import {Mensaje} from './Mensaje.js'
 /**Array de mensaje*/ // variable global
 
 var mensajes=new Array();
@@ -26,29 +28,33 @@ function actualizarMensajes(mensajes){
 /**
  * funcion que recoje los mensajes del uysuario y lo manda a la coleccion.
  * pagina de forma de texto dentro del div 
- * @param {type} mensaje de para añadir a la colecicon 
+ 
  * @returns {undefined}
  */         
             
             
- function enviarMensaje(mensaje){
+ function enviarMensaje(){
      // Recibe el texto del usuario 
      let mensaje=document.getElementById("msgText").value;
      
-     // lo añades a la coleccion de mensaje con la hora 
-     mensajes.add(new mensaje (mensaje,Date.now()));
+     // lo añades a la coleccion de mensaje con la hora push introducir en el arry 
+     //pop sacar del array.
+     mensajes.push(new Mensaje (mensaje, new Date()));
      
+     //limpar la caja de texto despues de enviar el mensaje 
+     document.getElementById("msgText").value="";
      
-     
+     //enfocamos la caja de texto del mensaje 
+     document.getElementById("msgText").focus();
  }
     
-
-
+//MANEJADORES DE EVENTOS.
 //asocio la funcion actualizar mensaje como manejadora del eveento .
 //Dom de la pagina. 
+//el dom es cuando cargas la pagina por primera vez
 
 document.addEventListener("DOMContentloaded", actualizarMensajes());
 
 //asocio la funcion ennviar mensaje como manejadora del evento.
 //click del evento send buttonn. 
-document.getElementById("senddbutton").addEventListenner('click', enviarMensaje);
+document.getElementById("sendButton").addEventListener('click', enviarMensaje);
--- HTML5Application/public_html/js/mensaje.js
+++ HTML5Application/public_html/js/mensaje.js
@@ -1,13 +1,13 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
 
-class Mensaje{
+class Mensaje{         
     // añidimos un metodo para ordenar los mensajes y la hora 
-    
-    texto="";
-    fecha;
-    
+    constructor(text,dateTime){
+        
+        this.text=text;
+        this.dateTime=dateTime;
+    }
     
 }
+
+// sirve para exportar a  la vista que queremos
+export {Mensaje};
